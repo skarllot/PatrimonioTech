e0df92362475ea19490104c5aa80606c60a1494b
Fabrício Godoy <skarllot@gmail.com>
2024-06-07 14:43:17 -0300

    test: Cover use cases

---
+++ tests/PatrimonioTech.App.Tests/Credentials/v1/AddUser/CredentialAddUserUseCaseTest.cs
@@ -0,0 +1,140 @@
+﻿using FluentAssertions;
+using JetBrains.Annotations;
+using NSubstitute;
+using PatrimonioTech.App.Credentials.v1.AddUser;
+using PatrimonioTech.App.Database;
+using PatrimonioTech.Domain.Credentials;
+using PatrimonioTech.Domain.Credentials.Actions.AddUser;
+using PatrimonioTech.Domain.Credentials.Services;
+
+namespace PatrimonioTech.App.Tests.Credentials.v1.AddUser;
+
+[TestSubject(typeof(CredentialAddUserUseCase))]
+public class CredentialAddUserUseCaseTest
+{
+    private readonly IDatabaseAdmin _databaseAdmin = Substitute.For<IDatabaseAdmin>();
+    private readonly IUserCredentialRepository _userCredentialRepository = Substitute.For<IUserCredentialRepository>();
+    private readonly IAddUserScenario _addUserScenario = Substitute.For<IAddUserScenario>();
+    private readonly IKeyDerivation _keyDerivation = Substitute.For<IKeyDerivation>();
+    private readonly CredentialAddUserUseCase _sut;
+
+    public CredentialAddUserUseCaseTest()
+    {
+        _sut = new CredentialAddUserUseCase(
+            _databaseAdmin,
+            _userCredentialRepository,
+            _addUserScenario,
+            _keyDerivation);
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenItIsValid_ThenSaveUser()
+    {
+        // Arrange
+        _addUserScenario
+            .Execute(Arg.Any<AddUserCredential>())
+            .Returns(new UserCredentialAdded("john", "123", "password", Guid.Empty, 1, 1));
+        _userCredentialRepository
+            .Add(Arg.Any<UserCredential>(), CancellationToken.None)
+            .Returns(Unit.Default);
+        _keyDerivation
+            .TryGetKey(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>())
+            .Returns("password");
+        _databaseAdmin
+            .CreateDatabase(Guid.Empty, Arg.Any<string>())
+            .Returns(Unit.Default);
+
+        // Act
+        var result = await _sut.Execute(new CredentialAddUserRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeOk();
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenScenarioFails_ThenReturnError()
+    {
+        // Arrange
+        _addUserScenario
+            .Execute(Arg.Any<AddUserCredential>())
+            .Returns(AddUserCredentialError.InvalidPassword.Of(PasswordError.TooShort));
+
+        // Act
+        var result = await _sut.Execute(new CredentialAddUserRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeErr().Should().BeOfType<CredentialAddUserError.BusinessError>();
+        _keyDerivation.ReceivedCalls().Should().BeEmpty();
+        _databaseAdmin.ReceivedCalls().Should().BeEmpty();
+        _userCredentialRepository.ReceivedCalls().Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenCryptographyFails_ThenReturnError()
+    {
+        // Arrange
+        _addUserScenario
+            .Execute(Arg.Any<AddUserCredential>())
+            .Returns(new UserCredentialAdded("john", "123", "password", Guid.Empty, 1, 1));
+        _userCredentialRepository
+            .Add(Arg.Any<UserCredential>(), CancellationToken.None)
+            .Returns(Unit.Default);
+        _keyDerivation
+            .TryGetKey(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>())
+            .Returns(GetKeyError.InvalidPassword);
+
+        // Act
+        var result = await _sut.Execute(new CredentialAddUserRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeErr().Should().BeOfType<CredentialAddUserError.CryptographyError>();
+        _databaseAdmin.ReceivedCalls().Should().BeEmpty();
+        _userCredentialRepository.ReceivedCalls().Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenDatabaseFails_ThenReturnError()
+    {
+        // Arrange
+        _addUserScenario
+            .Execute(Arg.Any<AddUserCredential>())
+            .Returns(new UserCredentialAdded("john", "123", "password", Guid.Empty, 1, 1));
+        _keyDerivation
+            .TryGetKey(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>())
+            .Returns("password");
+        _databaseAdmin
+            .CreateDatabase(Guid.Empty, Arg.Any<string>())
+            .Returns((CreateDatabaseError)0);
+
+        // Act
+        var result = await _sut.Execute(new CredentialAddUserRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeErr().Should().BeOfType<CredentialAddUserError.DatabaseError>();
+        _userCredentialRepository.ReceivedCalls().Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenRepositoryFails_ThenReturnError()
+    {
+        // Arrange
+        _addUserScenario
+            .Execute(Arg.Any<AddUserCredential>())
+            .Returns(new UserCredentialAdded("john", "123", "password", Guid.Empty, 1, 1));
+        _keyDerivation
+            .TryGetKey(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>())
+            .Returns("password");
+        _databaseAdmin
+            .CreateDatabase(Guid.Empty, Arg.Any<string>())
+            .Returns(Unit.Default);
+        _userCredentialRepository
+            .Add(Arg.Any<UserCredential>(), CancellationToken.None)
+            .Returns(UserCredentialAddError.NameAlreadyExists);
+
+        // Act
+        var result = await _sut.Execute(new CredentialAddUserRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeErr().Should().BeOfType<CredentialAddUserError.StorageError>();
+    }
+}
---
+++ tests/PatrimonioTech.App.Tests/Credentials/v1/GetUserInfo/CredentialGetUserInfoUseCaseTest.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using JetBrains.Annotations;
+using NSubstitute;
+using PatrimonioTech.App.Credentials.v1.GetUserInfo;
+using PatrimonioTech.Domain.Credentials;
+using PatrimonioTech.Domain.Credentials.Actions.AddUser;
+using PatrimonioTech.Domain.Credentials.Services;
+
+namespace PatrimonioTech.App.Tests.Credentials.v1.GetUserInfo;
+
+[TestSubject(typeof(CredentialGetUserInfoUseCase))]
+public class CredentialGetUserInfoUseCaseTest
+{
+    private readonly IUserCredentialRepository _userCredentialRepository = Substitute.For<IUserCredentialRepository>();
+    private readonly IKeyDerivation _keyDerivation = Substitute.For<IKeyDerivation>();
+    private readonly CredentialGetUserInfoUseCase _sut;
+
+    public CredentialGetUserInfoUseCaseTest()
+    {
+        _userCredentialRepository.GetAll(CancellationToken.None).Returns(CredentialsFixture);
+
+        _sut = new CredentialGetUserInfoUseCase(
+            _userCredentialRepository,
+            _keyDerivation);
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenUserExistsAndPasswordIsValid_ThenReturnsUserInfo()
+    {
+        // Arrange
+        _keyDerivation
+            .TryGetKey("password", Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>())
+            .Returns("secret");
+
+        // Act
+        var result = await _sut.Execute(new CredentialGetUserInfoRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeOk().DatabasePassword.Should().Be("secret");
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenUserDoesNotExists_ThenReturnsError()
+    {
+        // Act
+        var result = await _sut.Execute(new CredentialGetUserInfoRequest("laura", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeErr().Should().BeOfType<CredentialGetUserInfoError.UserNotFound>();
+    }
+
+    [Fact]
+    public async Task GivenARequest_WhenUserExistsAndPasswordIsInvalid_ThenReturnsError()
+    {
+        // Arrange
+        _keyDerivation
+            .TryGetKey("password", Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>())
+            .Returns(GetKeyError.InvalidPassword);
+
+        // Act
+        var result = await _sut.Execute(new CredentialGetUserInfoRequest("john", "password"), CancellationToken.None);
+
+        // Assert
+        result.Should().BeErr().Should().BeOfType<CredentialGetUserInfoError.CryptographyError>();
+    }
+
+    private static IReadOnlyList<UserCredential> CredentialsFixture =>
+    [
+        UserCredential.Create(new UserCredentialAdded("John", "123", "password", Guid.Empty, 1, 1)),
+        UserCredential.Create(new UserCredentialAdded("Carl", "456", "password", Guid.Empty, 2, 1)),
+        UserCredential.Create(new UserCredentialAdded("Anne", "789", "password", Guid.Empty, 3, 1)),
+    ];
+}
