7d3b5e1765677ca6ed0d126a52c93eaecc387761
Fabrício Godoy <skarllot@gmail.com>
2026-03-07 16:05:01 -0300

    chore: fix RectiveUI dependencies

--- src/PatrimonioTech.Gui/DependencyInjection/ViewLocator.cs
+++ src/PatrimonioTech.Gui/DependencyInjection/ViewLocator.cs
@@ -7,11 +7,31 @@ namespace PatrimonioTech.Gui.DependencyInjection;
 
 public sealed class ViewLocator : IViewLocator
 {
-    public IViewFor? ResolveView<T>(T? viewModel, string? contract = null) => viewModel switch
+    public IViewFor<TViewModel>? ResolveView<TViewModel>(string? contract = null) where TViewModel : class
+    {
+        if (typeof(TViewModel) == typeof(LoginViewModel))
+        {
+            return (IViewFor<TViewModel>)(IViewFor)new LoginView();
+        }
+
+        if (typeof(TViewModel) == typeof(UserCreateViewModel))
+        {
+            return (IViewFor<TViewModel>)(IViewFor)new UserCreateView();
+        }
+
+        if (typeof(TViewModel) == typeof(DashboardViewModel))
+        {
+            return (IViewFor<TViewModel>)(IViewFor)new DashboardView();
+        }
+
+        return null;
+    }
+
+    public IViewFor? ResolveView(object? instance, string? contract = null) => instance switch
     {
         LoginViewModel => new LoginView(),
         UserCreateViewModel => new UserCreateView(),
         DashboardViewModel => new DashboardView(),
-        _ => null
+        _ => null,
     };
 }
--- src/PatrimonioTech.Gui/Main/MainWindow.axaml.cs
+++ src/PatrimonioTech.Gui/Main/MainWindow.axaml.cs
@@ -1,5 +1,5 @@
-using Avalonia.ReactiveUI;
 using ReactiveUI;
+using ReactiveUI.Avalonia;
 
 namespace PatrimonioTech.Gui.Main;
 
